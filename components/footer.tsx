b7b5a666019cff44a60676c021fe32364ff61944
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-06-23 21:38:26 +0200

    Change the footer component

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,16 +3,16 @@ import { EXAMPLE_PATH } from '../lib/constants'
 
 const Footer = () => {
   return (
-    <footer className="bg-neutral-50 border-t border-neutral-200">
+    <footer className="border-t bg-neutral-50 border-neutral-200">
       <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
+        <div className="flex flex-col items-center py-28 lg:flex-row">
           <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
             Statically Generated with Next.js.
           </h3>
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
+          <div className="flex flex-col items-center justify-center lg:flex-row lg:pl-4 lg:w-1/2">
             <a
               href="https://nextjs.org/docs/basic-features/pages"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+              className="px-12 py-3 mx-3 mb-6 font-bold text-white transition-colors duration-200 bg-black border border-black hover:bg-white hover:text-black lg:px-8 lg:mb-0"
             >
               Read Documentation
             </a>
