a18e7face76e165fac7df92b9086e84930f8a124
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-06-23 21:43:40 +0200

    Update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -16,12 +16,7 @@ const Footer = () => {
             >
               Read Documentation
             </a>
-            <a
-              href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-              className="mx-3 font-bold hover:underline"
-            >
-              View on GitHub
-            </a>
+            
           </div>
         </div>
       </Container>
