f82656a7d9e43c19c8cc0ca7c7507f98a90ada4d
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-06-24 23:26:32 +0200

    Add the first post

--- components/alert.tsx
+++ components/alert.tsx
@@ -1,25 +1,42 @@
 import Container from './container'
 import cn from 'classnames'
+import { EXAMPLE_PATH } from '../lib/constants'
 
-type Props = {
-  preview?: boolean
-}
-
-const Alert = ({ preview }: Props) => {
+export default function Alert({ preview }) {
   return (
     <div
       className={cn('border-b', {
-        'bg-neutral-800 border-neutral-800 text-white': preview,
-        'bg-neutral-50 border-neutral-200': !preview,
+        'bg-accent-7 border-accent-7 text-white': preview,
+        'bg-accent-1 border-accent-2': !preview,
       })}
     >
       <Container>
         <div className="py-2 text-sm text-center">
-          
+          {preview ? (
+            <>
+              This page is a preview.{' '}
+              <a
+                href="/api/exit-preview"
+                className="underline transition-colors duration-200 hover:text-cyan"
+              >
+                Click here
+              </a>{' '}
+              to exit preview mode.
+            </>
+          ) : (
+            <>
+              The source code for this blog is{' '}
+              <a
+                href={`https://github.com/Piercel2022/${EXAMPLE_PATH}`}
+                className="underline transition-colors duration-200 hover:text-success"
+              >
+                available on GitHub
+              </a>
+              .
+            </>
+          )}
         </div>
       </Container>
     </div>
   )
 }
-
-export default Alert
