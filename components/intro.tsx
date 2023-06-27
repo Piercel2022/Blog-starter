f167744866a964590046d83607dc3d23256570e4
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-06-27 10:11:06 +0200

    Modify post content

--- components/intro.tsx
+++ components/intro.tsx
@@ -3,7 +3,7 @@ const Intro = () => {
   return (
     <section className="flex flex-col items-center mt-16 mb-16 md:flex-row md:justify-between md:mb-12">
       <h1 className="text-5xl leading-tight tracking-tighter text-center font-3 md:text-8xl md:pr-8">
-         React&Rails Blog.
+         Blog.
       </h1>
       <h4 className="mt-5 text-lg text-center md:text-left md:pl-8">
     
